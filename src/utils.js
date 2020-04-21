40c41163efe6e5e82559baf15790e7e03caadd09
Alex Elarbee <alex.elarbee@accesscfa.com>
2020-04-21 16:57:08 -0400

    Add vector operations Add trigonometric util functions

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,11 @@ const range = (start, stop, step = 1) =>
     Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step);
 
 const radian = degree => degree * (Math.PI / 180); // Convert a degree to a radian
+const degree = r => r * 180/Math.pi; // Degree from radian
+
+const coordToRadian = (x,y) => Math.atan2(y,x);
+
+const polarToCartesian = (r, degrees) => new Vector(r * Math.sin( degrees ), r * Math.cos(degrees));
 
 class Vector {
     constructor(x, y){
@@ -15,8 +20,23 @@ class Vector {
     static toHeading(r){
         return new Vector(Math.sin(r), Math.cos(r)*-1);
     }
+
+    multiply(v){
+        return new Vector(this.x * v.x, this.y * v.y);
+    }
+
+    add(v){
+        return new Vector(this.x + v.x, this.y + v.y);
+    }
+
+    subtract(v){
+        return new Vector(this.x - v.x, this.y - v.y);
+    }
 }
 
 exports.range = range;
 exports.radian = radian;
 exports.Vector = Vector;
+exports.coordToRadian = coordToRadian;
+exports.degree = degree;
+exports.polarToCartesian = polarToCartesian;
