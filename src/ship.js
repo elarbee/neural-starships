3a06286fe5a06e6d1ae10698ba868ceaa54b519b
Alex Elarbee <alex.elarbee@accesscfa.com>
2020-04-21 00:40:55 -0400

    Add draw function and accessors to ship.js

--- src/ship.js
+++ src/ship.js
@@ -1,18 +1,43 @@
 class Ship{
-    constructor(x, y, angle, speed, acc) {
-        this.x = y;
+    constructor(x, y, size) {
+        this.x = x;
         this.y = y;
-        this.angle = angle;
-        this.speed = speed;
-        this.acc = acc;
+        this.size = size;
+        this.angle = 90 * Math.PI / 180;
+        this.speed = 100;
+        this.acc = 50;
+        this.color;
     }
 
     draw(ctx){
+        console.log(this.x);
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
         ctx.beginPath();
-        ctx.moveTo(75, 50);
-        ctx.lineTo(100, 75);
-        ctx.lineTo(100, 25);
+        ctx.moveTo(0, 0 - this.size); // tip of rectangle
+        ctx.lineTo( this.size / 2, 0 + this.size ); // Right point
+        ctx.lineTo( -this.size / 2, 0 + this.size ); // left point
+        ctx.fillStyle = this.color;
         ctx.fill();
+
+        ctx.restore();
+    }
+
+    setAngle(a){
+       this.angle = a * Math.PI / 180;
+    }
+
+    setColor(c){
+        this.color = c;
+    }
+
+    getX(){
+        return this.x;
+    }
+
+    getY(){
+        return this.y;
     }
 }
 
