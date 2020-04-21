f18a09c6d9a40fde01bc31cb88b5e5e72599b9a6
Alex Elarbee <alex.elarbee@accesscfa.com>
2020-04-21 12:00:52 -0400

    Add sourcemaps to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,13 @@ module.exports = {
   entry: './src/starships.js',
   output: {
     filename: 'main.js',
+    sourceMapFilename: '[name].js.map',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000
-  }
+  },
+  devtool: 'source-map'
 };
