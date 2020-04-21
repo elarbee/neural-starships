6b520c71d96b66d785058315d052a2582fe2a610
Alex Elarbee <alex.elarbee@accesscfa.com>
2020-04-21 12:03:24 -0400

    Add vector class and radian helper function to utils

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,22 @@
 const range = (start, stop, step = 1) =>
-    Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step)
+    Array(Math.ceil((stop - start) / step)).fill(start).map((x, y) => x + y * step);
+
+const radian = degree => degree * (Math.PI / 180); // Convert a degree to a radian
+
+class Vector {
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+    }
+    get(){
+        return {x: this.x, y: this.y}
+    }
+
+    static toHeading(r){
+        return new Vector(Math.sin(r), Math.cos(r)*-1);
+    }
+}
 
 exports.range = range;
+exports.radian = radian;
+exports.Vector = Vector;
